cd92e5addf81f7760a6da647b0a4eb504f3bd42f
Asael Camejo <160143471+Asaelucla@users.noreply.github.com>
2025-02-07 09:41:58 -0400

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,29 +1,26 @@
-import cl_Articulo from "../Cl_Articulo";
-
 /**
- * En una bodega se tiene el costo de varios articulos  y se le piensa ganar a cada uno el 50%..
- * Hacer un programa que calcule el total de ganancia que tendra la bodega al venderlos todos.
- * Ej. Costos varios articulos:10, 20, 14,6 y 4
- * La salida requerida presenta el siguiente formato:
+ * En una Bodega se tiene el costo de varios articulos y se le piensa ganar a cada uno del 50%
+ * Hacer un programa que calcule el total de ganacia que tendra la bodega al vendedor todos,
+ * Ej. costo varios articulos: 10,2,14,6 y 4
+ * la Salida requerida presenta el siguiente formato:
  * la ganancia sera de Bs.27
- *
  */
-import cl_Articulo from "../Cl_Articulo.js";
-import cl_Bodega from "../cl._Bodega.js";
+import Cl_articulo from "./Cl_articulo.js";
+import Cl_bodega from "./Cl_bodega.js";
 
-let articulos1 = new cl_Articulo("articulos", 10);
-let articulos2 = new cl_Articulo("articulos", 20);
-let articulos3 = new cl_Articulo("articulos", 14);
-let articulos4 = new c1_Articulo("articulos", 6);
-let articulos5 = new cl_Articulo("articulos", 4);
+let articulo1 = new Cl_articulo("articulo 1", 10);
+let articulo2 = new Cl_articulo("articulo 2", 20);
+let articulo3 = new Cl_articulo("articulo 3", 14);
+let articulo4 = new Cl_articulo("articulo 4", 6);
+let articulo5 = new Cl_articulo("articulo 5", 4);
 
-let Bodega = new cl_Bodega();
+let bodega = new Cl_bodega();
 
-Bodega.procesararticulos(articulos1);
-Bodega.procesararticulos(articulos2);
-Bodega.procesararticulos(articulos3);
-Bodega.procesararticulos(articulos4);
-Bodega.procesararticulos(articulos5);
+bodega.procesararticulo(articulo1);
+bodega.procesararticulo(articulo2);
+bodega.procesararticulo(articulo3);
+bodega.procesararticulo(articulo4);
+bodega.procesararticulo(articulo5);
 
-const gananciaTotal = Bodega.procesararticulos();
+const gananciaTotal = bodega.totaldeGanancia();
 console.log(`La ganancia total es: ${gananciaTotal}`);
