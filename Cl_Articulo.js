de1661a2715817cd3c41f248bb6f1f1cd4672ef9
Asael Camejo <160143471+Asaelucla@users.noreply.github.com>
2025-02-07 09:43:00 -0400

    Update Cl_Articulo.js

--- Cl_Articulo.js
+++ Cl_Articulo.js
@@ -1,18 +1,28 @@
-export default class cl_Articulo {
-  constructor(costo) {
+export default class Cl_articulo {
+  constructor(costo, nombre) {
     this.costo = costo;
+    this.nombre = nombre;
   }
   calcularGanancia() {
     const porcentajeGanancia = 0.5;
     return this.costo * porcentajeGanancia;
   }
-  set costo(costo) {
-    this._costo = +costo;
+  setcosto(costo) {
+    this.costo += costo;
   }
-  get costo() {
-    return this._costo;
+  getcosto() {
+    return this.costo;
+  }
+  setnombre(nombre) {
+    return this.nombre += nombre;
+  }
+  getnombre() {
+    return this.nombre;
   }
   ganancia() {
     return this.costo / 2;
+  }
+  gananciaTotal(){
+    return gananciaTotal;
   }
 }
